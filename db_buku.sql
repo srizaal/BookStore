57a9e84dac5bfca2606bb1e133e88af2379576f1
srizaal <134043631+srizaal@users.noreply.github.com>
2023-08-01 13:53:48 +0700

    Database buku

---
+++ db_buku.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 01, 2023 at 08:52 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_buku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_buku`
+--
+
+CREATE TABLE `tb_buku` (
+  `kode_buku` int(11) NOT NULL,
+  `judul_buku` varchar(255) NOT NULL,
+  `tahun_terbit` year(4) DEFAULT NULL,
+  `kode_penerbit` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_buku`
+--
+
+INSERT INTO `tb_buku` (`kode_buku`, `judul_buku`, `tahun_terbit`, `kode_penerbit`) VALUES
+(12, 'Perjalanan Jauh', '2022', 1),
+(15, 'Jalan Jauh Jangan Lupa Pulang', '2023', 2),
+(16, 'Aku', '2022', 2),
+(17, 'Game Of Thrones', '2023', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_penerbit`
+--
+
+CREATE TABLE `tb_penerbit` (
+  `kode_penerbit` int(11) NOT NULL,
+  `nama_penerbit` varchar(255) NOT NULL,
+  `alamat_penerbit` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_penerbit`
+--
+
+INSERT INTO `tb_penerbit` (`kode_penerbit`, `nama_penerbit`, `alamat_penerbit`) VALUES
+(1, 'Gramedia Pustaka Utama', 'Jl. Palmerah Barat Jakarta'),
+(2, 'Mizan Pustaka', 'Jl. Cinambo 136 (Cisarantem Wetan) Bandung'),
+(3, 'Penerbit Erlangga', 'Jl. H. Baping Raya Ciracas Jakarta');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  ADD PRIMARY KEY (`kode_buku`),
+  ADD KEY `kode_penerbit` (`kode_penerbit`);
+
+--
+-- Indexes for table `tb_penerbit`
+--
+ALTER TABLE `tb_penerbit`
+  ADD PRIMARY KEY (`kode_penerbit`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  MODIFY `kode_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tb_penerbit`
+--
+ALTER TABLE `tb_penerbit`
+  MODIFY `kode_penerbit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  ADD CONSTRAINT `tb_buku_ibfk_1` FOREIGN KEY (`kode_penerbit`) REFERENCES `tb_penerbit` (`kode_penerbit`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
